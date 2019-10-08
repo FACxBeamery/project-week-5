f5f90ab070dd5f9622a13d66340b68e710534b9c
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-08 16:24:03 +0100

    db connected to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const router = require("./router.js");
+const initDb = require("../database/dbConnection").initDb;
+const getDb = require("../database/dbConnection").getDb;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,10 @@ app.use(formidable());
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}. Ready to accept requests!`);
-});
+initDb()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}. Ready to accept requests!`);
+        });
+    })
+    .catch(console.error);
