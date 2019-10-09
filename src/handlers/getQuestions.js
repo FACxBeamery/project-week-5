b49b4fe82f0d21b0eb08dc153d1bf2eff146d5ce
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-09 10:02:15 +0100

    queries are stored in a sensible way

--- src/handlers/getQuestions.js
+++ src/handlers/getQuestions.js
@@ -2,8 +2,6 @@ const readQuestions = require("../../database/queries/readQuestions");
 const getDB = require("../../database/dbConnection.js").getDb;
 
 const getQuestions = (req, res) => {
-    // const sortBy = req.query.sortBy;
-    // res.send(dbConnection(readQuestions));
     const db = getDB();
     res.send(readQuestions(db.collection("questions")));
 };
