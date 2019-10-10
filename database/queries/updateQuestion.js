eb4d17b55151674a9de65526739d0518aadacfe3
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-10 11:29:13 +0100

    id functionality fixed

--- database/queries/updateQuestion.js
+++ database/queries/updateQuestion.js
@@ -1,9 +1,15 @@
+const ObjectId = require("mongodb").ObjectId;
 const updateQuestion = (newAnswer, idToUpdate, questions) => {
+    console.log(typeof idToUpdate);
     return new Promise((resolve, reject) => {
-        questions.update({ _id: idToUpdate }, { $push: { answers: newAnswer } }, (err, result) => {
-            if (err) reject(err);
-            resolve(result);
-        });
+        questions.updateOne(
+            { _id: ObjectId(idToUpdate) },
+            { $push: { answers: newAnswer } },
+            (err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            }
+        );
     });
 };
 
